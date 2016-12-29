d82953afd02bf6b1b804fa2241b788de0bb0b357
Canna71 <gcannata@gmail.com>
2016-12-29 10:42:02 +0100

    second commit

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 // Module to control application life.
-const {app} = electron;
+const {app, globalShortcut} = electron;
 // Module to create native browser window.
 const {BrowserWindow} = electron;
 
@@ -11,6 +11,12 @@ let win;
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({width: 1200, height: 900});
+
+  let auguryPath = `C:\\Users\\gcannata\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\elgalmkoelokbchhkhacckoklkejnhcd\\1.2.7_0`;
+   BrowserWindow.addDevToolsExtension(auguryPath);
+    /*globalShortcut.register('Ctrl+Shift+i', ()=>{
+        mainWindow.webContents.toggleDevTools();
+    });*/
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);

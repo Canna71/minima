e162c31697a8d74f1e55ff0b8738a47400339729
Canna71 <gcannata@gmail.com>
2016-12-29 12:35:58 +0100

    scripts

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,28 @@ var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// var appRoot = 
+var scripts = ['./node_modules/lodash/lodash.min.js','./node_modules/jquery/dist/jquery.min.js'];
+
 module.exports = {
   devtool: 'source-map',
   debug: true,
 
   entry: {
+        'styles': ['./src/styles/minima.less'],
+    'scripts': scripts,
     'angular2': [
       'rxjs',
       'reflect-metadata',
       '@angular/core'
     ],
     'app': './src/main',
-    'styles': ['./src/styles/minima.less']
+
   },
 
   output: {
     path: __dirname + '/build/',
-    publicPath: 'build/',
+    publicPath: './',
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
     chunkFilename: '[id].chunk.js'
@@ -65,7 +70,15 @@ module.exports = {
         test: /\.less$/,
         include: [ /styles/ ],
        loaders: ['style-loader','css-loader',  'less-loader']
-      }
+      },
+      // load global scripts using script-loader
+       { include: [/lodash/,/jquery/], test: /\.js$/, loader: 'script-loader' },
+       { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(jpg|png|gif)$/, loader: 'url-loader?limit=10000' },
+      // { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.(otf|woff|ttf|svg)$/, loader: 'url?limit=10000' },
+      { test: /\.woff2$/, loader: 'url?limit=10000&mimetype=font/woff2' },
+      { test: /\.eot$/, loader: 'file' }
     ]
   },
 
@@ -74,8 +87,11 @@ module.exports = {
                 template: 'src/app/index.html',
                 chunksSortMode: 'dependency'
             }),
-    new CommonsChunkPlugin({ name: 'angular2', filename: 'angular2.js', minChunks: Infinity }),
-    new CommonsChunkPlugin({ name: 'common',   filename: 'common.js' }),
+     new CommonsChunkPlugin({ name: ['styles', 'scripts', 'app'].reverse() }),
+    //new CommonsChunkPlugin({ name: 'angular2', filename: 'angular2.js', minChunks: Infinity }),
+    //new CommonsChunkPlugin({ name: 'common',   filename: 'common.js' }),
+    // new CommonsChunkPlugin({ name: 'styles', filename: 'styles.js'  }),
+    // new CommonsChunkPlugin({ name: 'scripts', filename: 'scripts.js' }),
     new ExtractTextPlugin('app.bundle.css')
   ],
   target:'node-webkit',

0db434dbcb977dfb4e8f2db685c70d18ff7406cb
Canna71 <gcannata@gmail.com>
2016-12-30 17:17:51 +0100

    spellchecking

--- src/app/UI/main/main.component.ts
+++ src/app/UI/main/main.component.ts
@@ -2,6 +2,10 @@ import { StorageService } from '../../services/storage.service';
 import { PageComponent } from '../page/page.component';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
+// spell checking stuff
+var electron_spellchecker = (<any>window).require('electron-spellchecker');
+var { SpellCheckHandler, ContextMenuListener, ContextMenuBuilder } = electron_spellchecker;
+
 @Component({
     selector: 'm-main',
     template: require('./main.component.html'),
@@ -14,7 +18,18 @@ export class MainComponent implements OnInit {
     constructor(private storage: StorageService) { }
 
     ngOnInit() {
+        (<any>window).spellCheckHandler = new SpellCheckHandler();
+        (<any>window).spellCheckHandler.attachToInput();
+
+        // Start off as US English, America #1 (lol)
+        (<any>window).spellCheckHandler.switchLanguage('en-US');
+
+        let contextMenuBuilder = new ContextMenuBuilder((<any>window).spellCheckHandler);
+        let contextMenuListener = new ContextMenuListener((info) => {
+            contextMenuBuilder.showPopupMenu(info);
+        });
     }
+    
 
     load(path: string) {
         //see app2.coffee
